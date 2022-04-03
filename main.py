9f4b50891aea79c8c817c4f2a2ddca3ec1ce4b3d
seungw0n <34816905+seungw0n@users.noreply.github.com>
2022-04-03 19:28:33 +0900

    Create main.py and connect to PyQt5

---
+++ main.py
@@ -0,0 +1,18 @@
+import subprocess
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+
+form_class = uic.loadUiType("main.ui")[0]
+
+class WindowClass(QMainWindow, form_class):
+    def __init__(self):
+        super().__init__()
+        self.setupUi(self)
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    myWindow = WindowClass()
+    myWindow.show()
+    app.exec_()
