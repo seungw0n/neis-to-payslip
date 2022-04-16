0e9389f01ee8071e0b8a2839404fbb1e1c92877b
seungw0n <34816905+seungw0n@users.noreply.github.com>
2022-04-16 17:38:59 +0900

    Create excel.py

---
+++ src/excel.py
@@ -0,0 +1,30 @@
+import openpyxl
+from openpyxl.styles import Alignment
+
+
+def open_excel(filename, data_only=True):
+    # data_only = true then
+    try:
+        document = openpyxl.load_workbook(filename, data_only=data_only)
+        return document
+    except FileNotFoundError:
+        print("[open_excel] Cannot find: " + filename)
+        raise
+    # print(type(document))  # to test
+
+
+def save_excel(wb, filename):
+    wb.save(filename)
+
+
+def shift_right(sheet):
+    """ 금액 range 은 F17 - F44, H17 - H33"""
+    lst1 = ["F"+str(i) for i in range(17, 45)]
+    lst2 = ["H"+str(i) for i in range(17, 34)]
+    for s in lst1:
+        currentCell = sheet[s]
+        currentCell.alignment = Alignment(horizontal='right', vertical="center")
+
+    for s in lst2:
+        currentCell = sheet[s]
+        currentCell.alignment = Alignment(horizontal='right', vertical="center")
